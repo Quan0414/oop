115405072981a204f50cef9a57a26d5e0fbd9f5b
Quan0414 <151910063+Quan0414@users.noreply.github.com>
2024-12-16 19:30:45 +0700

    Delete ck2 directory

--- ck2/src/IGiangVien.java
+++
@@ -1,7 +0,0 @@
-public interface IGiangVien {
-    void addGiangVien(GiangVien gv);
-    void editGiangVien(GiangVien gv);
-    GiangVien searchGiangVien(String maCBNV);
-    Double getTotalSalary(String hocHamHocVi);
-    void display();
-}
